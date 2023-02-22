cae3bf25942498b5031bac3295f1e323f9150843
Yanguo Cao <CaoY35@cardiff.ac.uk>
2023-02-22 13:21:20 +0000

    optimize codes more readable and usable

--- prime.js
+++ prime.js
@@ -1,47 +1,43 @@
-function determinePrime(number) {
-    let isPrime = true;
-    let factors = [];
-    for(let j=2;j<number;j++){
-        if(number%j == 0){
-            isPrime = false;
-            factors.push(j);
-        }
-    }
-    if(isPrime == true){
-       return("Prime!");
-    }
-    else{
-        return(factors);
-    }
+function getFactors(number, firstOnly) {
+    const factors = [];
+    for(let i = 2; i < number; i++)
+        if(number % i == 0)
+        {
+            factors.push(i)
+            if (firstOnly)
+                break
+        } 
+    return factors;
 }
 
-function determinePrimeInRange(start,end) {
-    let primes = [];
-    for(let i=start;i<=end;i++){
-        let isPrime = true;
-        for(let j=2;j<i;j++){
-            if(i%j == 0){
-                isPrime = false;
-                break;
-            }
-        }
-        if(isPrime == true){
-            primes.push(i);
-        }
-    }
-    return primes;
+function isPrime(number) {
+    return !getFactors(number, true).length;
 }
 
-const inputNumber = document.getElementById("input-number");
-const startNumber = document.getElementById("start");
-const endNumber = document.getElementById("end");
+function getPrimesInRange(start, end) {
+    let primes = []
+    for(let i = start; i <= end; i++)
+        if (isPrime(i))
+            primes.push(i)
+    return primes
+}
+
+function showResult(result) {
+    // document.getElementById("result").innerText = result;
+    alert(result);
+}
 
 const determinePrimeBtn = document.getElementById("factor-button");
 const determinePrimeInRangeBtn = document.getElementById("range-button");
-const result = document.getElementById("result");
+
 determinePrimeBtn.addEventListener("click", function(){
-    result.innerText = determinePrime(inputNumber.value);
+    const num = document.getElementById("input-number").value
+    const result = isPrime(num) ? 'Prime!' : getFactors(num);
+    showResult(result);
 });
 determinePrimeInRangeBtn.addEventListener("click", function(){
-    result.innerText = determinePrimeInRange(start.value,end.value);
+    const startNumber = document.getElementById("start").value;
+    const endNumber = document.getElementById("end").value;
+    const result = getPrimesInRange(startNumber,endNumber);
+    showResult(result);
 });
